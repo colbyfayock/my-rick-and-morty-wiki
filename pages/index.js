fd959ac66a51900d2fcff9130925d8979ab8db32
colbyfayock <fayock@gmail.com>
2020-07-05 11:20:23 -0400

    Step 2: dynamically listing our our characters from the API

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ data }) {
-  console.log('data', data);
+  const { results = [] } = data;
   return (
     <div className="container">
       <Head>
@@ -23,42 +23,26 @@ export default function Home({ data }) {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Wubba Lubba Dub Dub!
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Rick and Morty Character Wiki
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className="grid">
+          {results.map(result => {
+            const { id, name, image } = result;
+            return (
+              <li key={id} className="card">
+                <a href="#">
+                  <img src={image} alt={`${name} Thumbnail`} />
+                  <h3>{ name }</h3>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
       </main>
 
       <footer>
@@ -159,6 +143,9 @@ export default function Home({ data }) {
 
           max-width: 800px;
           margin-top: 3rem;
+          list-style: none;
+          margin-left: 0;
+          padding-left: 0;
         }
 
         .card {
