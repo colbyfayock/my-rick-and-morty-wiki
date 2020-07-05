3850e08e47654053d33f8440557e882e3579b335
colbyfayock <fayock@gmail.com>
2020-07-05 10:55:49 -0400

    Step 1: fetching Rick and Morty character data serverside

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import Head from 'next/head'
 
-export default function Home() {
+const defaultEndpoint = `https://rickandmortyapi.com/api/character/`;
+
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndpoint)
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default function Home({ data }) {
+  console.log('data', data);
   return (
     <div className="container">
       <Head>
